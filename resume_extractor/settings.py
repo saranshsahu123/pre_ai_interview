ad95c6c93708c9ca6bbf288d5cb34c1ec578617e
saranshsahu123 <saranshsahu532@gmail.com>
2025-11-16 11:08:57 +0530

    remove some folder and file

--- resume_extractor/settings.py
+++ resume_extractor/settings.py
@@ -123,10 +123,10 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = BASE_DIR / 'media'
 
 
 
